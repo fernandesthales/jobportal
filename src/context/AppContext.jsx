689aee295e218c79658ea4e78abe4f1eac5b440a
Thales <thalesgomes2@gmail.com>
2025-02-13 22:07:27 -0300

    updated everything

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -3,11 +3,19 @@ import { createContext, useState } from "react";
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
+
+    const [searchFilter, setSearchFilter] = useState({
+        title:'',
+        location:''
+    })
+
+    const [isSearched, setIsSearched] = useState(false); // Fixed typo
     const [state, setState] = useState(null); // Example state
 
     const value = {
-        state,
-        setState
+        state, setState,
+        setSearchFilter, searchFilter,
+        isSearched, setIsSearched // Fixed typo
     };
 
     return (
