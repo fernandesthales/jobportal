b793dd6d5a71ef3200cfd0bc2ab73598f5aa8f9b
Thales <thalesgomes2@gmail.com>
2025-03-08 18:56:40 -0300

    mais uma

--- server/config/instrument.js
+++ server/config/instrument.js
@@ -12,13 +12,10 @@ Sentry.init({
 */
 
 import * as Sentry from "@sentry/node";
-import { Http } from "@sentry/node/dist/integrations/http";
+
 
 Sentry.init({
   dsn: "https://c3053de9c053aac7f4ba406473d73315@o4508933036507136.ingest.us.sentry.io/4508933038669824",
-  integrations: [
-    new Http({ tracing: true })   // Correct usage for Http
-  ],
   tracesSampleRate: 1.0  // Full tracing for debugging (adjust in production)
 });
 
