f0c5a8a573094ddb3b491f213c29a1649a5a8b24
Thales <thalesgomes2@gmail.com>
2025-03-08 11:56:27 -0300

    novo commit mais novo

--- server/models/User.js
+++ server/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     name:{type:String, required: true},
     email:{type:String, required:true},
     resume: {type:String},
-    image:{type: String, required:true}
+    image:{type: String}
 })
 
 const User = mongoose.model('User', userSchema)
