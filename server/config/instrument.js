c0b6763131e0ca6a250ade2c0adbc37ac8a615a7
Thales <thalesgomes2@gmail.com>
2025-03-08 18:49:45 -0300

    ajuste mongo

--- server/config/instrument.js
+++ server/config/instrument.js
@@ -12,14 +12,12 @@ Sentry.init({
 */
 
 import * as Sentry from "@sentry/node";
-import { Mongo } from "@sentry/node/dist/integrations/mongo";
 import { Http } from "@sentry/node/dist/integrations/http";
 
 Sentry.init({
   dsn: "https://c3053de9c053aac7f4ba406473d73315@o4508933036507136.ingest.us.sentry.io/4508933038669824",
   integrations: [
-    new Http({ tracing: true }),   // Correct usage for Http
-    new Mongo({ useMongoose: true })  // Correct usage for MongoDB with Mongoose
+    new Http({ tracing: true })   // Correct usage for Http
   ],
   tracesSampleRate: 1.0  // Full tracing for debugging (adjust in production)
 });
